d4f7827455e4f003bc1c12b9db10fd4dab659ebd
Danny Oancea <doancea@wiser.com>
2022-01-28 14:03:52 -0500

    MOBEE-1075: Converted all custom matchers

--- WiseMock/NimbleMatchers.swift
+++ WiseMock/NimbleMatchers.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Nimble
 
+//Invocation Count
 public func invoke<T: Mockable>(_ name: T.MockedMethod, times: Int = 1) -> Predicate<T> {
     
     return Predicate<T>.define { actualExpression in
@@ -21,50 +22,62 @@ public func invoke<T: Mockable>(_ name: T.MockedMethod, times: Int = 1) -> Predi
 
 }
 
-/// Equatable
-public func invoke<T: Mockable, E: Equatable>(_ name: T.MockedMethod, atInvocation invocationIndex: Int = 0, withParameter parameter: E?, at parameterIndex: Int = 0) -> MatcherFunc<T> {
-    return MatcherFunc { actualExpression, failureMessage in
-
-        failureMessage.postfixMessage = "invoke \(name) with \(parameter) at index \(parameterIndex) on invocation \(invocationIndex)"
-
-        if let mockable = try actualExpression.evaluate() {
+//Equality
+public func invoke<T: Mockable, E: Equatable>(_ name: T.MockedMethod, atInvocation invocationIndex: Int = 0, withParameter parameter: E?, at parameterIndex: Int = 0) -> Predicate<T> {
+    return Predicate<T>.define { actualExpression in
+        var expectationMessage: ExpectationMessage!
+        var status: PredicateStatus!
+        
+        if let mockable = try? actualExpression.evaluate() {
             let actualParameter: E? = mockable.parameter(for: name, at: parameterIndex, andInvocation: invocationIndex)
-            failureMessage.actualValue = "\(actualParameter)"
-            return actualParameter == parameter
+            let actualMessage = String(describing: actualParameter)
+            
+            expectationMessage = .expectedActualValueTo(actualMessage)
+            status = PredicateStatus(bool: actualParameter == parameter)
+        } else {
+            status = .fail
+            expectationMessage = .fail("something else happened")
         }
-
-        return false
+        return PredicateResult(status: status, message: expectationMessage)
     }
+
 }
 
-/// Identity
-public func invoke<T: Mockable, E: AnyObject>(_ name: T.MockedMethod, atInvocation invocationIndex: Int = 0, withIdenticalParameter parameter: E?, at parameterIndex: Int = 0) -> MatcherFunc<T> {
-    return MatcherFunc { actualExpression, failureMessage in
-
-        failureMessage.postfixMessage = "invoke \(name) with \(parameter) at index \(parameterIndex) on invocation \(invocationIndex)"
-
-        if let mockable = try actualExpression.evaluate() {
+//Identity
+public func invoke<T: Mockable, E: AnyObject>(_ name: T.MockedMethod, atInvocation invocationIndex: Int = 0, withIdenticalParameter parameter: E?, at parameterIndex: Int = 0) -> Predicate<T> {
+    return Predicate<T>.define { actualExpression in
+        var expectationMessage: ExpectationMessage!
+        var status: PredicateStatus!
+        
+        if let mockable = try? actualExpression.evaluate() {
             let actualParameter: E? = mockable.parameter(for: name, at: parameterIndex, andInvocation: invocationIndex)
-            failureMessage.actualValue = "\(actualParameter)"
-            return actualParameter === parameter
+            let actualMessage = String(describing: actualParameter)
+            
+            expectationMessage = .expectedActualValueTo(actualMessage)
+            status = PredicateStatus(bool: actualParameter === parameter)
+        } else {
+            status = .fail
+            expectationMessage = .fail("something else happened")
         }
-
-        return false
+        return PredicateResult(status: status, message: expectationMessage)
     }
 }
 
 /// Closure Matcher
-public func invoke<T: Mockable, U>(_ name: T.MockedMethod, atInvocation invocationIndex: Int = 0, atParameterIndex parameterIndex: Int = 0, withMatcher matcher: @escaping ((U?) -> Bool?)) -> MatcherFunc<T> {
-    return MatcherFunc { actualExpression, failureMessage in
-
-        failureMessage.postfixMessage = "invoke \(name) with matching parameter at index \(parameterIndex) on invocation \(invocationIndex)"
-
-        if let mockable = try actualExpression.evaluate() {
-            let actualValue: U? = mockable.parameter(for: name, at: parameterIndex, andInvocation: invocationIndex)
-            failureMessage.actualValue = "parameter as \(actualValue)"
-            return matcher( actualValue ) ?? false
+public func invoke<T: Mockable, U>(_ name: T.MockedMethod, atInvocation invocationIndex: Int = 0, atParameterIndex parameterIndex: Int = 0, withMatcher matcher: @escaping ((U?) -> Bool)) -> Predicate<T> {
+    return Predicate<T>.define { actualExpression in
+        var expectationMessage: ExpectationMessage!
+        var status: PredicateStatus!
+        
+        if let mockable = try? actualExpression.evaluate() {
+            let actualParameter: U? = mockable.parameter(for: name, at: parameterIndex, andInvocation: invocationIndex)
+            
+            expectationMessage = .fail("parameter as \(String(describing: actualParameter)) does not match")
+            status = PredicateStatus(bool: matcher(actualParameter))
+        } else {
+            status = .fail
+            expectationMessage = .fail("something else happened")
         }
-
-        return false
+        return PredicateResult(status: status, message: expectationMessage)
     }
 }
