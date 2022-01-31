834316547590e56e56a7108d5071c92f3e9dcc48
Danny Oancea <doancea@wiser.com>
2022-01-31 13:16:52 -0500

    MOBEE-1075: Package updates

--- WiseMock/Package.swift
+++ WiseMock/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "WiseMock",
+    platforms: [
+          .macOS(.v10_10), .iOS(.v9), .tvOS(.v9)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,14 +15,17 @@ let package = Package(
             targets: ["WiseMock"]),
     ],
     dependencies: [
-        .target
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMinor(from: "9.2.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WiseMock",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Nimble", package: "Nimble",
+                                         condition: .when(platforms: [.macOS, .iOS])),
+            ]),
         .testTarget(
             name: "WiseMockTests",
             dependencies: ["WiseMock"]),
